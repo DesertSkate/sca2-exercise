3b3e07b06caa5e2c20e42f1196ed141d87b01f89
DesertSkate <robertschaz@gmail.com>
2019-07-18 17:19:46 -0400

    Finished site with all reqs

--- 4_AM/src/components/PseudoForm/subComponents/SubmitText/index.js
+++ 4_AM/src/components/PseudoForm/subComponents/SubmitText/index.js
@@ -3,7 +3,8 @@ import {Text} from "./styles"
 
 const SubmitText = props => (
     <Text>User's name is {props.data["name"]}, and {props.data["pronoun"]} is from {props.data["Hometown"]}. {props.data["pronoun"]} was born on {props.data["birthday"]}, 
-    and {props.data["pronoun"]} favorite foods is {props.data.FavoriteFoods[0]}, {props.data.FavoriteFoods[1]}, and {props.data.FavoriteFoods[2]}.</Text>
+    and {props.data["pronoun"].toLowerCase() === "he" ? "his" : props.data["pronoun"].toLowerCase().includes("she") ? "her" : ""} favorite foods are&nbsp;
+    {props.data.FavoriteFoods[0]}, {props.data.FavoriteFoods[1]}, and {props.data.FavoriteFoods[2]}.</Text>
 )
 
 export default SubmitText
